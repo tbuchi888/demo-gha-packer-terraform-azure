8564885993776d51205a64efb35dac9a23108909
Takuya Kawabuchi <tbuchi888@users.noreply.github.com>
2024-07-18 13:29:33 +0900

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-terraform"
+    resource_group_name  = "demo-terraform-gha"
     storage_account_name = "tbuchi888tfstate2407"
     container_name       = "backendcontainer"
     key                  = "packer-custom-vm-terraformv990v3.tfstate"
